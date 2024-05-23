269e094cbcc1cf95479d9606a1ec53d07603f724
Oscar-Wohlfarhrt <oscarwohlfarhrt@gmail.com>
2024-05-23 19:14:25 -0300

    Create CreateDataBase.sql

---
+++ SQL Scripts/Database Creation/CreateDataBase.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE TBD2024;
+
+USE master;
+DROP Table dbo.RanNames;
+
+USE TBD2024;
+CREATE TABLE dbo.RanNames(
+    Id INT PRIMARY KEY IDENTITY,
+    Name VARCHAR(50),
+    Type INT
+);
+
+INSERT INTO dbo.RanNames (Name,Type)
+VALUES ("Test",0);
